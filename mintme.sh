e327d93abab13dff8129dd9285a2c23cfee9d38f
masihgurutkj <49093218+masihgurutkj@users.noreply.github.com>
2023-01-22 00:38:09 +0700

    Create mintme.sh

---
+++ mintme.sh
@@ -0,0 +1,1 @@
+./webchain-miner -o pool.webchain.network:2222 -u 0x15482e6553bc529b632a74540597cecb1be2279f -p x --cpu-priority=1 --worker-id=Ubuntu --no-huge-page --cpu-affinity=1 --threads=2 --donate-level=1
